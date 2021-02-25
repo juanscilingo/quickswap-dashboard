e30580cd502515a2f9225604e8cc032b411e0bb2
Juan Manuel Scilingo <juanscilingo@gmail.com>
2021-02-25 06:04:05 -0300

    Fixed connect button when account is disconnected from metamask

--- src/components/Metamask/ConnectButton.js
+++ src/components/Metamask/ConnectButton.js
@@ -8,8 +8,8 @@ const ConnectButton = props => {
   const web3React = useWeb3React();
 
   if (!(window.web3 || window.ethereum)) return <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn" target="_blank" rel="noopener noreferrer"><Button>Install Metamask</Button></a>
-  else if (web3React.chainId !== ChainId.MATIC) return <Badge color="var(--red)" large>Unsupported Network</Badge>
   else if (!web3React.active) return <Button onClick={() => web3React.activate(injected, console.error)}>Connect</Button>
+  else if (web3React.chainId !== ChainId.MATIC) return <Badge color="var(--red)" large>Unsupported Network</Badge>
   else return <Button onClick={web3React.deactivate}>Disconnect</Button>
 }
 
