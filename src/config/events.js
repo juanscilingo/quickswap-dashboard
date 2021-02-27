9c67869f3068e65e01d12e4116f5575fdf508462
Juan Manuel Scilingo <juanscilingo@gmail.com>
2021-02-27 20:07:40 -0300

    Ignore header not found errors

--- src/config/events.js
+++ src/config/events.js
@@ -2,10 +2,14 @@ const IGNORED_REASONS = [
   'invalid ENS name'
 ]
 
+const IGNORED_ERROR_CODES = [
+  -32603
+]
+
 const configureGlobalErrorHandler = () => {
   if ('onunhandledrejection' in window)
     window.onunhandledrejection = e => { 
-      if (IGNORED_REASONS.includes(e?.reason?.reason))
+      if (IGNORED_REASONS.includes(e?.reason?.reason) || IGNORED_ERROR_CODES.includes(e?.reason?.code))
         return;
 
       console.log('Unhandled Error', e)
